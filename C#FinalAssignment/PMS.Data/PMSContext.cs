431ce9a009673cf238fe894017861518c266b0e7
Chandni <chandwanic3008@gmail.com>
2021-04-02 20:23:15 +0530

    Assignment Submitted

---
+++ C#FinalAssignment/PMS.Data/PMSContext.cs
@@ -0,0 +1,15 @@
+﻿using PMS.Data.Models;
+using System.Data.Entity;
+
+namespace PMS.Data
+{
+    public class PMSContext: DbContext
+    {
+        public PMSContext() : base("PMSEntities")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
